8801875349585454ab286197f5a1888b7e3fc906
Ali Ahmar <aliahmarawan8@gmail.com>
2023-07-11 20:57:59 +0500

    Updates

--- src/container/Chats/message/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ProfileHead from "./ProfileHead";
-import ReplyChat from "./ReplyChat";
-
-const Profile = ({ productName }) => {
-  return <ProfileHead chatHeader={productName} />;
-};
-
-export default Profile;
